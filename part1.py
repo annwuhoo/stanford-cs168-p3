f900a740214ebb08314e68e8c9566cdf622c8a1b
Ann Wu <wuyuanchih@gmail.com>
2020-04-28 09:42:17 -0700

    add SGD

--- part1.py
+++ part1.py
@@ -25,7 +25,8 @@ def leastSquares(X, y):
     XT = X.transpose()
     return np.dot(np.dot(np.linalg.inv(np.dot(XT,X)),XT), y)
 
-def gradientDescent(X, y_noise, y_true, niter, alpha):
+def gradientDescent(X, y_noise, y_true, alpha):
+    niter = 20
     a_gd = np.zeros((d,1))
     for i in range(niter):
         error = sq_err(a_gd, X, y_noise)
@@ -34,8 +35,17 @@ def gradientDescent(X, y_noise, y_true, niter, alpha):
         print("i:", i, "; total error:",total_sq_err(a_gd, X, y_true))
     return total_sq_err(a_gd, X, y_true)
 
-def SGD(X, y):
-    return
+def SGD(X, y_noise, y_true, alpha):
+    niter = 1000
+    a_gd = np.zeros((d,1))
+    for i in range(niter):
+        idx = np.random.randint(d, size=1) # pick a random datapoint
+        error = sq_err(a_gd[idx], X[idx], y_noise[idx])
+        #gradient = np.dot(X.transpose(), error)/n
+        a_gd[idx] = a_gd[idx] - alpha*X[idx]*error
+        if (i%100 == 0):
+            print("i:", i, "; total error:",total_sq_err(a_gd, X, y_true))
+    return total_sq_err(a_gd, X, y_true)
 
 def main():
     # Generate data (given)
@@ -44,17 +54,17 @@ def main():
     y_true = X.dot(a_true)
     y_noise = X.dot(a_true) + np.random.normal(0,0.5,size=(n,1))
 
-    # Part a
+    # --- Part a
     a_ls = leastSquares(X, y_noise)
     a_zeros = np.zeros((d,1))
     print("err_ls:", total_sq_err(a_ls, X, y_true))
     print("err_zeros:", total_sq_err(a_zeros, X, y_true))
 
-    # Part b
+    # --- Part b
     niter = 20
-    #err_gd_1 = gradientDescent(X, y_noise, y_true, niter, 0.00005)
-    err_gd_2 = gradientDescent(X, y_noise, y_true, niter, 0.0005)
-    #err_gd_3 = gradientDescent(X, y_noise, y_true, niter, 0.0007)
+    #err_gd_1 = gradientDescent(X, y_noise, y_true, 0.00005)
+    err_gd_2 = gradientDescent(X, y_noise, y_true, 0.0005)
+    #err_gd_3 = gradientDescent(X, y_noise, y_true, 0.0007)
 
     #print("1:", err_gd_1)
     print("2:", err_gd_2)
@@ -62,6 +72,8 @@ def main():
 
     #plt.bar([1,2,3], [err_gd_00005,err_gd_0005,err_gd_0007])
     #plt.show()
+
+    # --- Part c
 
 
 main()
